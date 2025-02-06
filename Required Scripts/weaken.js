7996207bf68859e00ef09614a1f2bec3e504b72d
Loaf <168995949+1LoafaBread@users.noreply.github.com>
2025-02-05 23:16:25 -0500

    Update weaken.js

--- Required Scripts/weaken.js
+++ Required Scripts/weaken.js
@@ -1,31 +1,4 @@
 export async function main(ns) {
     var server = ns.args[0];
-    var type = ns.args[1];
-
-    async function hackServer(server) {
-        var weakenAmount = await ns.weaken(server);
-
-        ns.tprint(`${ns.getHostname()} :: Weakened Server '${server}' Security Level By ${weakenAmount.toFixed(3)} (Current Security LVL: ${ns.getServerSecurityLevel(serverToAttack).toFixed(3)}) (Min: ${ns.getServerMinSecurityLevel(serverToAttack)})`);
-        
-        if (ns.getServerSecurityLevel(server) == ns.getServerMinSecurityLevel(server)) {
-            ns.tprint(`${ns.getHostname()} :: Server '${server}' Reached Lowest Security Level.`);
-        }
-        
-        await ns.sleep(2000);
-    }
-
-    if (type == "all") {
-        while (true) {
-            if (ns.getServerMoneyAvailable(server) > ns.getServerMaxMoney(server) * 0.01) {
-                await hackServer(server);
-            } else {
-                ns.tprint(`${ns.getHostname()} :: Insufficient Money on '${server}'.`);
-                break;
-            }
-        }
-    } else if (type == "walk") {
-        await hackServer(server);
-    } else {
-        ns.tprint(`Unknown Type: ${type}`);
-    }
+    await ns.weaken(server);
 }
